6b9b9ec7fd6907e1059456751ae6bb70e048950b
Tabitha Head <tabithahead@Tabithas-MacBook-Pro.local>
2021-03-17 03:32:24 -0400

    deleted not working notebook, added happiness dataframe to database

---
+++ Tabitha-Happiness/schema.sql
@@ -0,0 +1,19 @@
+-- happiness table
+
+CREATE TABLE happiness (
+	country VARCHAR PRIMARY KEY,
+	happiness INT,
+	happiness_score INT,
+	gdp_per_capita INTEGER,
+	life_expectancy INTEGER,
+	freedom INTEGER,
+	generosity INTEGER,
+	dystopia_residual INTEGER
+);
+
+SELECT * FROM happiness;
+
+-- suicide table
+
+
+-- human development table
